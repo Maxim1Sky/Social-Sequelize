b170f42df7374e75aa20b4c1a3dd450048d39101
Maxim Toma <maxim.toma@sky.uk>
2023-11-12 13:46:40 +0000

    Initialised that databse and created User table

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,15 @@
+const { Model, DataTypes, Sequelize } = require("sequelize");
+const path = require("path");
 
+const db = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+  logging: false,
+  define: {
+    timestamps: false,
+  },
+});
 
+db.authenticate();
 
-
-
-module.exports = {
-    db,
-    
-}
+module.exports = { db, DataTypes, Model };
