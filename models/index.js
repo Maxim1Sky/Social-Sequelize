c937418ee012b7463c427c05893c4c565352710b
Maxim Toma <maxim.toma@sky.uk>
2023-11-12 14:16:30 +0000

    Moved all associations to a differnet index.js

--- models/index.js
+++ models/index.js
@@ -4,18 +4,6 @@ const Post = require("./Post");
 const Profile = require("./Profile");
 const User = require("./User");
 
-User.hasOne(Profile);
-Profile.belongsTo(User);
-
-User.hasMany(Post);
-Post.belongsTo(User);
-
-Post.hasMany(Comment);
-Comment.belongsTo(Post);
-
-User.belongsToMany(Like, { through: "userLikes" });
-Like.belongsToMany(User, { through: "userLikes" });
-
 module.exports = {
   Comment,
   Like,
